6291b00aa71074839357d2c9e1768542b19143d1
florian <florianEBI@users.noreply.github.com>
2014-08-18 11:12:20 +0000

    added model documentation annotations (swagger)

--- src/main/java/uk/ac/ebi/pride/archive/web/service/model/project/ProjectDetail.java
+++ src/main/java/uk/ac/ebi/pride/archive/web/service/model/project/ProjectDetail.java
@@ -1,6 +1,8 @@
 package uk.ac.ebi.pride.archive.web.service.model.project;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
 import uk.ac.ebi.pride.archive.web.service.model.contact.ContactDetail;
 
 import java.io.Serializable;
@@ -13,32 +15,48 @@ import java.util.Set;
  * @since 1.0.6
  */
 @SuppressWarnings("UnusedDeclaration")
+@ApiModel(value = "ProjectDetail", description = "Detailed information about a project")
 public class ProjectDetail extends ProjectSummary implements Serializable {
 
+    @ApiModelProperty(value = "the Digital Object Identifier (DOI) for the project (if available)")
     private String doi;
+    @ApiModelProperty(value = "details of the submitter of the dataset")
     private ContactDetail submitter;
-    private Set<ContactDetail> lapHeads;
+    @ApiModelProperty(value = "the Lab-Head or PI associated to the dataset")
+    private Set<ContactDetail> labHeads;
+    @ApiModelProperty(value = "the date the project has been submitted")
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd", timezone="GMT")
     private Date submissionDate;
+    @ApiModelProperty(value = "annotation to indicate that the dataset is a re-analysis based on other public data")
     private String reanalysis;
+    @ApiModelProperty(value = "the type(s) of experiment preformed")
     private Set<String> experimentTypes;
+    @ApiModelProperty(value = "the quantification method(s) used with the dataset (if any)")
     private Set<String> quantificationMethods;
+    @ApiModelProperty(value = "relevant keywords associated with the project")
     private String keywords;
 
-
+    @ApiModelProperty(value = "roject meta-data: information about the sample processing")
     private String sampleProcessingProtocol;
+    @ApiModelProperty(value = "project meta-data: information about the data processing")
     private String dataProcessingProtocol;
 
+    @ApiModelProperty(value = "links to other datasets related to this project (if available)")
     private String otherOmicsLink;
 
+    @ApiModelProperty(value = "project statistics: number of reported proteins")
     private int numProteins;
+    @ApiModelProperty(value = "project statistics: number of reported peptides")
     private int numPeptides;
+    @ApiModelProperty(value = "project statistics: number of spectra")
     private int numSpectra;
+    @ApiModelProperty(value = "project statistics: number of unique peptides")
     private int numUniquePeptides;
+    @ApiModelProperty(value = "project statistics: number of identified spectra")
     private int numIdentifiedSpectra;
 
     public ProjectDetail() {
-        this.lapHeads = new HashSet<ContactDetail>();
+        this.labHeads = new HashSet<ContactDetail>();
         this.experimentTypes = new HashSet<String>();
     }
 
@@ -58,12 +76,12 @@ public class ProjectDetail extends ProjectSummary implements Serializable {
         this.submitter = submitter;
     }
 
-    public Set<ContactDetail> getLapHeads() {
-        return lapHeads;
+    public Set<ContactDetail> getLabHeads() {
+        return labHeads;
     }
 
-    public void setLapHeads(Set<ContactDetail> lapHeads) {
-        this.lapHeads = lapHeads;
+    public void setLabHeads(Set<ContactDetail> labHeads) {
+        this.labHeads = labHeads;
     }
 
     public Date getSubmissionDate() {
